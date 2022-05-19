3b3604e7284ef8b33265726c5b3511f246fd6dec
Pavel_Boldyrev <pavel_boldyrev@epam.com>
2022-05-19 16:17:38 +0400

    hotfix #9

--- setup/sqlinit.sql
+++ setup/sqlinit.sql
@@ -1,121 +1,78 @@
-﻿-- Table: public.users
-
--- DROP TABLE IF EXISTS public.users;
-
-CREATE TABLE IF NOT EXISTS public.users
+﻿create table if not exists users
 (
-    user_id integer NOT NULL DEFAULT nextval('users_user_id_seq'::regclass),
-    username character varying(30) COLLATE pg_catalog."default" NOT NULL,
-    email character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    password character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    firstname character varying(25) COLLATE pg_catalog."default",
-    lastname character varying(35) COLLATE pg_catalog."default",
-    CONSTRAINT users_pkey PRIMARY KEY (user_id)
-)
+    user_id   serial
+        constraint users_pkey
+            primary key,
+    username  varchar(30)  not null,
+    email     varchar(50)  not null,
+    password  varchar(100) not null,
+    firstname varchar(25),
+    lastname  varchar(35)
+);
 
-TABLESPACE pg_default;
+alter table users
+    owner to postgres;
 
-ALTER TABLE IF EXISTS public.users
-    OWNER to postgres;
-
--- Table: public.user_roles
-
--- DROP TABLE IF EXISTS public.user_roles;
-
-CREATE TABLE IF NOT EXISTS public.user_roles
+create table if not exists items
 (
-    role_id integer NOT NULL DEFAULT nextval('user_roles_role_id_seq'::regclass),
-    role_title character varying(15) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT user_roles_pk PRIMARY KEY (role_id)
-)
+    item_id     serial
+        constraint "PK_items$item_id"
+            primary key,
+    title       varchar(60),
+    description varchar(300)
+);
 
-TABLESPACE pg_default;
+alter table items
+    owner to postgres;
 
-ALTER TABLE IF EXISTS public.user_roles
-    OWNER to postgres;
-
--- Table: public.users_user_roles
-
--- DROP TABLE IF EXISTS public.users_user_roles;
-
-CREATE TABLE IF NOT EXISTS public.users_user_roles
+create table if not exists lots
 (
-    users_user_id integer NOT NULL,
-    userroles_role_id integer NOT NULL,
-    CONSTRAINT users_user_roles_pkey PRIMARY KEY (users_user_id, userroles_role_id),
-    CONSTRAINT fkop9wfhgqfh0ejfxjimdlc3jxc FOREIGN KEY (userroles_role_id)
-        REFERENCES public.user_roles (role_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION,
-    CONSTRAINT fks79bm3bk3u022txmw1djc5s3h FOREIGN KEY (users_user_id)
-        REFERENCES public.users (user_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.users_user_roles
-    OWNER to postgres;
-
--- Table: public.lots
-
--- DROP TABLE IF EXISTS public.lots;
-
-CREATE TABLE IF NOT EXISTS public.lots
-(
-    lot_id integer NOT NULL DEFAULT nextval('"LOTS_lot_id_seq"'::regclass),
+    lot_id      integer default nextval('"LOTS_lot_id_seq"'::regclass) not null
+        constraint "LOTS_pkey"
+            primary key,
     start_price double precision,
-    stop_date date NOT NULL,
-    user_id integer NOT NULL,
-    bid_id integer,
-    item_id integer NOT NULL,
-    bid_inc double precision,
-    isactive boolean,
-    CONSTRAINT "LOTS_pkey" PRIMARY KEY (lot_id),
-    CONSTRAINT "FK_lots$items$item_id" FOREIGN KEY (item_id)
-        REFERENCES public.items (item_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-    CONSTRAINT "FK_lots$users$bid_id" FOREIGN KEY (bid_id)
-        REFERENCES public.users (user_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-    CONSTRAINT "FK_lots$users$user_id" FOREIGN KEY (user_id)
-        REFERENCES public.users (user_id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
+    stop_date   date                                                   not null,
+    user_id     integer                                                not null
+        constraint "FK_lots$users$user_id"
+            references users,
+    bid_id      integer
+        constraint "FK_lots$users$bid_id"
+            references users,
+    item_id     integer                                                not null
+        constraint "FK_lots$items$item_id"
+            references items,
+    bid_inc     double precision,
+    isactive    boolean
+);
 
-TABLESPACE pg_default;
+alter table lots
+    owner to postgres;
 
-ALTER TABLE IF EXISTS public.lots
-    OWNER to postgres;
--- Index: fki_o
+create index if not exists fki_o
+    on lots (user_id);
 
--- DROP INDEX IF EXISTS public.fki_o;
-
-CREATE INDEX IF NOT EXISTS fki_o
-    ON public.lots USING btree
-    (user_id ASC NULLS LAST)
-    TABLESPACE pg_default;
-
--- Table: public.items
-
--- DROP TABLE IF EXISTS public.items;
-
-CREATE TABLE IF NOT EXISTS public.items
+create table if not exists user_roles
 (
-    item_id integer NOT NULL DEFAULT nextval('items_item_id_seq'::regclass),
-    title character varying(60) COLLATE pg_catalog."default",
-    description character varying(300) COLLATE pg_catalog."default",
-    CONSTRAINT "PK_items$item_id" PRIMARY KEY (item_id)
-)
+    role_id    serial
+        constraint user_roles_pk
+            primary key,
+    role_title varchar(15) not null
+);
 
-TABLESPACE pg_default;
+alter table user_roles
+    owner to postgres;
 
-ALTER TABLE IF EXISTS public.items
-    OWNER to postgres;
+create table if not exists users_user_roles
+(
+    users_user_id     integer not null
+        constraint fks79bm3bk3u022txmw1djc5s3h
+            references users,
+    userroles_role_id integer not null
+        constraint fkop9wfhgqfh0ejfxjimdlc3jxc
+            references user_roles,
+    constraint users_user_roles_pkey
+        primary key (users_user_id, userroles_role_id)
+);
+
+alter table users_user_roles
+    owner to postgres;
